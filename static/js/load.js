b13cc42e4d27ff18a138097aaa03615787dce31c
William Sandbrink <wilsandbrink@gmail.com>
2019-08-10 22:32:11 +0200

    update code style

--- static/js/load.js
+++ static/js/load.js
@@ -1,22 +1,22 @@
-var s = document.getElementById( "server-name" );
-var statusc = document.getElementById( "status" );
-var body = document.getElementsByTagName( "body" )[0];
+var s = document.getElementById("server-name");
+var statusc = document.getElementById("status");
+var body = document.getElementsByTagName("body")[0];
 
-var map = document.getElementById( "map" );
-var gm = document.getElementById( "gamemode" );
-var mp = document.getElementById( "maxplayers" );
+var map = document.getElementById("map");
+var gm = document.getElementById("gamemode");
+var mp = document.getElementById("maxplayers");
 
 body.style.backgroundRepeat = "repeat";
 body.style.backgroundSize = "cover";
 
 
-function GameDetails( servername, serverurl, mapname, maxplayers, steamid, gamemode ) {
-	s.innerText = servername;
-	map.innerText += " " + mapname;
-	gm.innerText  += " " + gamemode;
-	mp.innerText  += " " + maxplayers;
+function GameDetails(servername, serverurl, mapname, maxplayers, steamid, gamemode) {
+  s.innerText = servername;
+  map.innerText += " " + mapname;
+  gm.innerText  += " " + gamemode;
+  mp.innerText  += " " + maxplayers;
 }
 
 function SetStatusChanged( status ) {
-	statusc.innerText = status;
+  statusc.innerText = status;
 }
