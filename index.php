aebc53c3bebeeb1318d8329a895e155f857bd640
William Sandbrink <wilsandbrink@gmail.com>
2019-10-18 22:16:26 +0200

    Forgot my copyright notice

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
+/*
+ * Copyright (C) 2019  William Sandbrink
+ *
+ * gmod-load-screen is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gmod-load-screen is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gmod-load-screen.  If not, see <https://www.gnu.org/licenses/>.
+ */
+?>
+<?php
 require_once("config.php");
 // $steamid = isset($_GET["steamid"]);
 if (isset($_GET["steamid"])) {
