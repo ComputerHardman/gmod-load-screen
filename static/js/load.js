5fa60d6d8075dea03204e87d2775ca5765e5c6db
William Sandbrink <wilsandbrink@gmail.com>
2020-06-05 20:00:57 +0200

    convert indentation to spaces

--- static/js/load.js
+++ static/js/load.js
@@ -27,12 +27,12 @@ body.style.backgroundRepeat = "repeat";
 body.style.backgroundSize = "cover";
 
 function GameDetails( servername, serverurl, mapname, maxplayers, steamid, gamemode ) {
-	s.innerText = servername;
-	map.innerText += " " + mapname;
-	gm.innerText  += " " + gamemode;
-	mp.innerText  += " " + maxplayers;
+    s.innerText = servername;
+    map.innerText += " " + mapname;
+    gm.innerText  += " " + gamemode;
+    mp.innerText  += " " + maxplayers;
 }
 
 function SetStatusChanged( status ) {
-	statusc.innerText = status;
+    statusc.innerText = status;
 }
